d9b8dd870cd58f983cf307686e380307e23e1a1c
Bhavika Bharti <45075050+noelle224@users.noreply.github.com>
2022-04-02 22:37:28 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -7,12 +7,12 @@ import 'firebase/compat/firestore';
 // write in your own firebase configurations.
 
   const firebaseApp = firebase.initializeApp({
-    apiKey: "AIzaSyDv0d7uKAyrHui2ueybOnn5Sd8vxNuZ3V8",
-    authDomain: "facebook-messenger-clone-fb01b.firebaseapp.com",
-    projectId: "facebook-messenger-clone-fb01b",
-    storageBucket: "facebook-messenger-clone-fb01b.appspot.com",
-    messagingSenderId: "668702949196",
-    appId: "1:668702949196:web:9c21d53a21863cbc6399e9"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "acom",
+    messagingSenderId: "66196",
+    appId: "1:6699e9"
   });
   
   const db= firebaseApp.firestore();
